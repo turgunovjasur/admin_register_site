c3405b36d48b075aca1553854df326af4a14dc7c
Jasur <tjasur224@gmail.com>
2023-12-30 10:02:48 +0500

    full

--- adminapp/views.py
+++ adminapp/views.py
@@ -13,7 +13,7 @@ def login_required_decorator(func):
 @login_required_decorator
 def logout_page(request):
     logout(request)
-    return redirect('login_page')
+    return redirect("login_page")
 
 
 def login_page(request):
@@ -25,7 +25,7 @@ def login_page(request):
             login(request, user)
             return redirect('home_page')
 
-    return render(request, 'login_html')
+    return render(request, 'login.html')
 
 
 @login_required_decorator
@@ -36,4 +36,4 @@ def home_page(request):
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login_page')
-    tamplate_name = 'signup.html'
+    template_name = 'signup.html'
